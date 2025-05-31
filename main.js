02feae860557ddfb5397b42d5aabe53d17c415c6
Choon Hoong <lauchoonhoong17@gmail.com>
2025-05-31 10:19:30 +0800

    Work in progress, fetched pet data

--- main.js
+++ main.js
@@ -6,3 +6,14 @@ async function start() {
 }
 
 start()
+
+async function petsArea() {
+  const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+  const petsData = await petsPromise.json()
+  petsData.forEach((pets) => {
+    console.log(pets.name)
+
+  })
+}
+
+petsArea()
